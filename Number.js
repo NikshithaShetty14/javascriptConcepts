9d4ad77f8167be34b6075cd3c4e71bf5bd1000b1
NikshithaShetty14 <nishashetty958@gmail.com>
2024-11-26 16:58:57 +0530

    number

--- Number.js
+++ Number.js
@@ -1,25 +1,25 @@
-// //Numbers
-// //1)toString() 2)toFixed(1) 3)toLocalString()
+ //Numbers
+ //1)toString() 2)toFixed(1) 3)toLocalString()
 
-// //tostring()
-// //It converts arry boolean number into an string.you need to check the type
- let score = 400;
-// let finalScore = score.toString(); //it won't update the value that is the reason we store in a variable
-// console.log(typeof finalScore); //string
+ //tostring()
+ //It converts arry boolean number into an string.you need to check the type
+let score = 400;
+let finalScore = score.toString(); //it won't update the value that is the reason we store in a variable
+console.log(typeof finalScore); //string
 
 // //other way
-// let balance = new Number(200); //here it specifies that it is a number //[Number: 200]
-// console.log(balance);
-// console.log(balance.toString()); //200(if you check the typeof the number has been converted to a string)(then you can use the string properties)
+let balance = new Number(200); //here it specifies that it is a number //[Number: 200]
+console.log(balance);
+console.log(balance.toString()); //200(if you check the typeof the number has been converted to a string)(then you can use the string properties)
 
-// //toFixed(1) 
-// //In toFixed you need to give the properties of number
-// let score1 = 40.23564;
-// console.log(score1.toFixed(2));//40.24
-// console.log(score.toFixed(2)); //400.00
+ //toFixed(1) 
+ //In toFixed you need to give the properties of number
+let score1 = 40.23564;
+console.log(score1.toFixed(2));//40.24
+console.log(score.toFixed(2)); //400.00
 
-// console.log(score1.toPrecision(2)); //40
-// console.log(score.toPrecision(3)); //400
+console.log(score1.toPrecision(2)); //40
+console.log(score.toPrecision(3)); //400
 
 //toLocalString()
 let  hundreds = 1000000;
