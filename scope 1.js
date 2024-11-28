23d811774220a511de624358283788c741c0e1ab
NikshithaShetty14 <nishashetty958@gmail.com>
2024-11-28 17:25:06 +0530

    scope

--- scope 1.js
+++ scope 1.js
@@ -13,9 +13,9 @@ let a = 300;
 if(true){
     let a = 20;
     const s = 15;
-    console.log("Inner", a)
+    console.log("Inner", a) //Inner 20 //Inside this block  the a value will be 20 only since it is a block scope
 
 }
 
-console.log("global", a);
+console.log("global", a); //global 300 //it is a global scope
 
